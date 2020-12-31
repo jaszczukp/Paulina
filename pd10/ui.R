d01c9c1552e381d291ef9cadecfa977f1a431123
jaszczukp <56119908+jaszczukp@users.noreply.github.com>
2020-12-31 15:54:01 +0100

    Add files via upload

---
+++ pd10/ui.R
@@ -0,0 +1,26 @@
+library(DT)
+library(plotly)
+library(shiny)
+library(shinydashboard)
+
+pageWithSidebar(
+  headerPanel("TIMSS visualization"),
+  sidebarPanel(
+    radioButtons("subject", "Subject:",
+                 list("Math", "Science")),
+    checkboxGroupInput("checkGroup", label = ("Year"), 
+                       choices = c("2015", "2019"),
+                       selected = "2019"),
+    selectInput("country_selection",
+                label = "Country:",
+                choices = M4_countries$Country,
+                multiple = TRUE)),
+  mainPanel(
+    plotOutput("plot_1"),
+    fluidRow(
+      column(width = 12,
+             box(
+               title = "Score and percentiles", width = NULL, status = "primary",
+               div(style = 'overflow-x: scroll', DT::dataTableOutput('table_1'))
+             ))))
+)

d851ad335659087a1c804b52a302d421521c4194
jaszczukp <56119908+jaszczukp@users.noreply.github.com>
2020-12-31 15:52:16 +0100

    Delete hosting.R

--- pd10/hosting.R
+++
@@ -1,5 +0,0 @@
-rsconnect::setAccountInfo(name='bajcyk',
-                          token='20BDD5219DF034835AD90BA1587C4791',
-                          secret='22inEX/JaBb/Eg6LGHxNwbAyg6c75vNalT9W1wjX')
-
-deployApp(appName = "pd1011", account = "bajcyk")
